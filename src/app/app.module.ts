3444464c8595443e9fc9772f866fab80c3f5fb14
RinKusChi <meyuzak@gmail.com>
2021-12-22 13:58:13 +0300

    components+models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    NaviComponent
+    NaviComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
